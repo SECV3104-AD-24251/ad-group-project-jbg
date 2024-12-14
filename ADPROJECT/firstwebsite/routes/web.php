6bb79698e902ede2f95d7be174a556b57104b3c2
mhdhfzi <hafizimacho8@gmail.com>
2024-12-14 16:15:10 +0800

    login and home

--- ADPROJECT/firstwebsite/routes/web.php
+++ ADPROJECT/firstwebsite/routes/web.php
@@ -1,10 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\VenueController;
+use App\Http\Controllers\HomeController;
 
-Route::view('/home', 'home');
+// Authentication Routes
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login.form');
+Route::post('/login', [LoginController::class, 'login'])->name('login');
 
+// Home Route - Displays the list of venues (secured by auth middleware)
+Route::get('/home', [HomeController::class, 'index'])->name('home')->middleware('auth');
 
-Auth::routes();
+// Show the user's bookings (secured by auth middleware)
+Route::get('/my-bookings', [VenueController::class, 'showBookings'])->name('my.bookings')->middleware('auth');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Book a venue (secured by auth middleware)
+Route::post('/book-venue/{venueId}', [VenueController::class, 'bookVenue'])->name('book.venue')->middleware('auth');
+
