c61ca160550bc61b75266359638f5a7199ed8f02
Muhammad Farezy Bin Ab Rahman <149126204+Farezyrahman@users.noreply.github.com>
2024-12-29 22:25:33 +0800

    update 3 back to previous
    This reverts commit c106f3d2c2e50f5025e8b2565269a9299a0904f1.

--- ADPROJECT/firstwebsite/database/seeders/DatabaseSeeder.php
+++ ADPROJECT/firstwebsite/database/seeders/DatabaseSeeder.php
@@ -2,22 +2,63 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\coach;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        // User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        \App\Models\User::factory()->create([
+            'name' => 'ADMIN',
+            'email' => 'admin@admin.com',
+            'role' => 'admin',
+            'password' => bcrypt('admin'),
+        ]);
+
+         // Create an example admin account
+         \App\Models\User::factory()->create([
+        'name' => 'Admin Example',
+        'email' => 'admin1@gmail.com',
+        'role' => 'admin',
+        'password' => bcrypt('123456789'), // Replace 'securepassword' with your preferred password
+        ]);
+
+        // making coach users
+        \App\Models\User::factory()->create([
+            'name' => 'COACH',
+            'email' => 'coach@coach.com',
+            'role' => 'coach',
+            'password' => bcrypt('coach'),
+        ]);
+        // filling the coach table
+
+        \App\Models\User::factory()->create([
+            'name' => 'COACH 1',
+            'email' => 'coach@gmail.com',
+            'role' => 'coach',
+            'password' => bcrypt('123456789'),
+        ]);
+        // f
+        coach::factory()->create([
+            'name' => 'Zakaria El Azzouzi',
+            'user_id' => 2,
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
+            'image' => 'vectimg.png',
+            'sport' => 'football',
+            'price' => 100,
+            'location' => 'Casablanca',
+            'status' => 'available',
+            'rating' => 5,
+            'yearsofexperience' => 5,
         ]);
     }
 }
--- ADPROJECT/firstwebsite/resources/views/layouts/app.blade.php
+++ ADPROJECT/firstwebsite/resources/views/layouts/app.blade.php
@@ -1,96 +1,63 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="icon" href="https://cdn.pixabay.com/photo/2016/05/26/14/11/chef-1417239_1280.png" />
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <title>{{ config('app.name', 'OSCM') }}</title>
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    </head>
+    <body class="font-sans antialiased " style="background-color: #F8EDE3"
+    >
+        <div class="min-h-screen " style="background-color: #F8EDE3"
+        >
+            @include('layouts.navigation')
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+            <!-- Page Heading -->
+            @if (isset($header))
+                <header class=" " style="">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                        {{ $header }}
+                    </div>
+                </header>
+            @endif
 
-    <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-</head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'OSCM') }}
-                </a>
-                <a class="navbar-brand" href="{{ url('/students') }}">
-                    Student
-                </a>
-                <a class="navbar-brand" href="{{ url('/subjects') }}">
-                    Subject
-                </a>
-                <a class="navbar-brand" href="{{ url('/lecturers') }}">
-                    Lecturer
-                </a>
-                <a class="navbar-brand" href="{{ url('/assessments') }}">
-                    Assessment
-                </a>
-                <a class="navbar-brand" href="{{ url('/venues') }}">
-                   Venue
-                </a>
+            <!-- Page Content -->
+            <main>
+                <style>
+                  
+     .active>.page-link, .page-link.active {
+                     background-color: #85586F;
+                     border-color: #878787;
+}
+                   
 
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+.page-link {
+    position: relative;
+    display: block;
+    padding: var(--bs-pagination-padding-y) var(--bs-pagination-padding-x);
+    font-size: var(--bs-pagination-font-size);
+    color: white;
+    text-decoration: none;
+    background-color: #766c71 ;
+    border: var(--bs-pagination-border-width) solid var(--bs-pagination-border-color);
+}
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
-</body>
+                </style>
+                {{ $slot }}
+                
+            </main>
+        </div>
+    </body>
 </html>
